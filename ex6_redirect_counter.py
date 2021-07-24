6faec11bf8717a6bc607928e268f56454634dca2
skbif <skbif@mail.ru>
2021-07-24 23:33:49 +0300

    ex6 ver1

--- ex6_redirect_counter.py
+++ ex6_redirect_counter.py
@@ -3,7 +3,7 @@ import requests
 try:
     response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
     if response.status_code == 200:
-        print(f"Конечный URL:{response.history[len(response.history)-1].url}")
+        print(f"Конечный URL: {response.history[len(response.history)-1].url}")
         print(f"Переходов до {response.history[len(response.history)-1].url}: {len(response.history)}")
         print(f"Всего переходов: {(len(response.history))+1}")
     else:
