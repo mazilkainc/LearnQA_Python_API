3d60bb77a78a712b4737318d722a1f1f2c033ab2
skbif <skbif@mail.ru>
2021-07-31 14:59:25 +0300

    ex11 ver1

---
+++ ex11_cookie.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
+print(response.cookies)
+
+class TestCookie:
+
+    def test_cookie(self):
+        url = "https://playground.learnqa.ru/api/homework_cookie"
+        response = requests.get(url)
+
+        assert response.status_code == 200, "Wrong response code"
+        response_cookie = response.cookies
+        print(response_cookie)
+        assert "HomeWork" in response_cookie, "There is no Cookie HomeWork"
+        expected_value = "hw_value"
+        actual_value = response_cookie["HomeWork"]
+        print(actual_value)
+        assert actual_value == expected_value, f"There is \"{actual_value}\" instead of \"{expected_value}\""
+
